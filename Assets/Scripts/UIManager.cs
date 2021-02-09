da7bcbee9572b895f5a8c4bc042567482daebad8
ronaldgameking <ronaldgameking@users.noreply.github.com>
2021-02-09 11:08:34 +0100

    Update UIManager.cs

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager Instance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
