e1be1d12a5ad151ca9b1ca497d4baacdfdb4a452
ronaldgameking <ronaldgameking@users.noreply.github.com>
2021-02-09 11:05:34 +0100

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections;
+using System.Numerics;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    //Trick from Eddie to prevent use of FindObjectOfType<> and GameObject.Find()
+    //Trick from Eddie (International Game Jam Dev, Sweden)
+    //to prevent use of FindObjectOfType<> and GameObject.Find()
     public static GameManager Instance;
 
     public Int32 Score { get; private set; } = 0;
