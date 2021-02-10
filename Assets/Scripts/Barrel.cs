e9082601cbe828f38c4e79b0b39de4e077fead54
ronaldgameking <ronaldgameking@users.noreply.github.com>
2021-02-10 22:27:13 +0100

    Added more stuff

--- Assets/Scripts/Barrel.cs
+++ Assets/Scripts/Barrel.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Barrel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
