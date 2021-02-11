51aeeaf6b6ec29dc83d71551265f490451ea4b3c
ronaldgameking <ronaldgameking@users.noreply.github.com>
2021-02-11 12:42:17 +0100

    I lost track

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform followThis;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = followThis.position + offset;
+    }
+}
