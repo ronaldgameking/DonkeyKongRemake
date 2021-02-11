a5227b4f9e4695a8692e35db14a965e6afa1e1e2
ronaldgameking <ronaldgameking@users.noreply.github.com>
2021-02-11 14:12:12 +0100

    Barrel

--- Assets/Scripts/Barrel.cs
+++ Assets/Scripts/Barrel.cs
@@ -11,4 +11,12 @@ public class Barrel : MonoBehaviour
             Destroy(collision.gameObject);
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.ChangeScore(10);
+        }
+    }
 }
