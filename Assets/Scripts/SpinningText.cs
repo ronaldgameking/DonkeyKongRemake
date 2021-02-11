849a689a7b7f025743d69b955b221a65d1748a45
ronaldgameking <ronaldgameking@users.noreply.github.com>
2021-02-11 17:32:26 +0100

    Main menu stuff

---
+++ Assets/Scripts/SpinningText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningText : MonoBehaviour
+{
+    [SerializeField] [ReadOnly] float timer;
+
+    public float maxSpeed = 4f;
+    public float resetTo = 1f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.back * timer);
+        timer += Time.deltaTime;
+        if (timer > maxSpeed)
+        {
+            timer = resetTo;
+        }
+    }
+}
---
+++ Assets/Scripts/TextEasing.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextEasing : MonoBehaviour
+{
+    public TextMeshProUGUI EasingText;
+
+    public int MaxSize = 24;
+    public int MinSize = 14;
+    public int CurrentSize = 24;
+
+    bool hasPeaked = true;
+    bool hasLowed = false;
+
+    float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= 0.02f)
+        {
+            if (hasPeaked && !hasLowed)
+            {
+                CurrentSize--;
+            }
+            else if (!hasPeaked && hasLowed)
+            {
+                CurrentSize++;
+            }
+
+            if (CurrentSize >= MaxSize)
+            {
+                hasPeaked = true;
+                hasLowed = false;
+            }
+
+            if (CurrentSize <= MinSize)
+            {
+                hasPeaked = false;
+                hasLowed = true;
+            }
+            timer = 0f;
+        }
+        timer += Time.deltaTime;
+        EasingText.fontSize = CurrentSize;
+        
+    }
+}
